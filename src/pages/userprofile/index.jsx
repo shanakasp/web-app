70ad2d24f52046d406937740b922111c9247e24a
Shahbaz-Tanveer <shanakaprince@gmail.com>
2024-11-16 14:42:46 +0530

    modified index.jsx in userprofile

--- src/pages/userprofile/index.jsx
+++ src/pages/userprofile/index.jsx
@@ -12,41 +12,43 @@ const Index = () => {
   };
 
   return (
-    <div className="max-w-7xl mx-auto pt-[4%]">
-      <hr class="relative left-1/2 right-1/2 -mx-[49vw] w-[98vw] border-t-2 border-gray-200 mt-3" />
-      {/* Navigation Tabs */}
-      <div className="flex justify-center items-center">
-        <div className="pt-8 max-w-md w-full">
-          <div className="flex gap-2 justify-center">
-            <button
-              onClick={() => handleTabClick("profile")}
-              className={`px-4 pt-2 border-b-2 text-lg font-medium ${
-                activeTab === "profile"
-                  ? "border-emerald-500 border-solid text-[#049c64] bg-emerald-100"
-                  : "border-none text-[#979797]" // Non-selected button color
-              }`}
-            >
-              Profile Details
-            </button>
-            <button
-              onClick={() => handleTabClick("savedJobs")}
-              className={`px-4 pt-2 border-b-2 text-lg font-medium ${
-                activeTab === "savedJobs"
-                  ? "border-emerald-500 border-solid text-[#049c64] bg-emerald-100"
-                  : "border-none text-[#979797]" // Non-selected button color
-              }`}
-            >
-              Saved Jobs
-            </button>
+    <div className="max-w-full pt-[5%]  ">
+      <div className="w-6xl  mx-auto px-[7%] ">
+        <hr class="relative left-1/2 right-1/2 -mx-[49vw] w-[98vw] border-t-2 border-gray-200 mt-3" />
+        {/* Navigation Tabs */}
+        <div className="flex justify-center items-center">
+          <div className="pt-8 max-w-md w-full">
+            <div className="flex gap-2 justify-center">
+              <button
+                onClick={() => handleTabClick("profile")}
+                className={`px-4 pt-2 border-b-2 text-lg font-medium ${
+                  activeTab === "profile"
+                    ? "border-emerald-500 border-solid text-[#049c64] bg-emerald-100"
+                    : "border-none text-[#979797]" // Non-selected button color
+                }`}
+              >
+                Profile Details
+              </button>
+              <button
+                onClick={() => handleTabClick("savedJobs")}
+                className={`px-4 pt-2 border-b-2 text-lg font-medium ${
+                  activeTab === "savedJobs"
+                    ? "border-emerald-500 border-solid text-[#049c64] bg-emerald-100"
+                    : "border-none text-[#979797]" // Non-selected button color
+                }`}
+              >
+                Saved Jobs
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-      <hr class="relative left-1/2 right-1/2 -mx-[49vw] w-[98vw] border-t-2 border-gray-200 " />
+        <hr class="relative left-1/2 right-1/2 -mx-[49vw] w-[98vw] border-t-2 border-gray-200 " />
 
-      {/* Render the active component based on the state */}
-      <div className="mt-6">
-        {activeTab === "profile" && <ProfileDetails />}
-        {activeTab === "savedJobs" && <SavedJobs />}
+        {/* Render the active component based on the state */}
+        <div className="mt-6">
+          {activeTab === "profile" && <ProfileDetails />}
+          {activeTab === "savedJobs" && <SavedJobs />}
+        </div>
       </div>
     </div>
   );
