7e794f9470265d0b217224e1d5c8efaa9a64e969
Shahbaz-Tanveer <shanakaprince@gmail.com>
2024-11-20 14:47:25 +0530

    modified Wavedesign.jsx in Landingpage

--- src/pages/Landingpage/Wavedesign.jsx
+++ src/pages/Landingpage/Wavedesign.jsx
@@ -5,14 +5,17 @@ const AnimatedWaves = () => {
   return (
     <div className="relative w-full h-32">
       {/* Fourth Wave now First (previously bottom) - 80% opacity */}
-      <div className="absolute inset-0 w-full h-full" style={{ opacity: 0.8 }}>
+      <div
+        className="mt-2 absolute inset-0 w-full h-full"
+        style={{ opacity: 0.8 }}
+      >
         <Wave
           fill="url(#gradient4)"
           options={{
-            height: 45, // Height of the wave itself
+            height: 35, // Height of the wave itself
             amplitude: 40, // Amplitude of the wave
             speed: 0.112, // Positive speed for left to right movement
-            points: 3,
+            points: 4,
           }}
         >
           <defs>
@@ -26,16 +29,16 @@ const AnimatedWaves = () => {
 
       {/* Third Wave now Second - 70% opacity */}
       <div
-        className="absolute inset-0 w-full h-full mt-1"
+        className="absolute inset-0 w-full h-full mt-2"
         style={{ opacity: 0.7 }}
       >
         <Wave
           fill="url(#gradient3)"
           options={{
-            height: 45, // Height of the wave itself
+            height: 35, // Height of the wave itself
             amplitude: 40, // Amplitude of the wave
             speed: 0.102, // Positive speed for left to right movement
-            points: 5,
+            points: 6,
           }}
         >
           <defs>
